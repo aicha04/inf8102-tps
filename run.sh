ec1e2ba56a3538d82b7b4c73d7757f5ffb995d0a
aicha04 <miloudi.ai04@gmai.com>
2022-11-07 23:36:02 -0500

    setup instance for appserver

--- run.sh
+++ run.sh
@@ -24,3 +24,15 @@ curl https://raw.githubusercontent.com/aicha04/inf8102-tps/main/setupInstance.sh
 subnetId=subnet-01cd3fe0dc6eba810
 M4Large="$(aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type m4.large --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupInstance.sh --subnet-id $subnetId --query "Instances[].[InstanceId]" --output text)"
 echo $M4Large
+
+ImageId=ami-08c40ec9ead489470 
+SecurityGroup=sg-0b90b622bf5484b42
+SubnetId=subnet-03fd421f03f28be62
+curl https://raw.githubusercontent.com/aicha04/inf8102-tps/main/setupInstanceAppserver.sh > setupInstance.sh
+curl https://raw.githubusercontent.com/aicha04/inf8102-tps/main/volume.json > volume.json
+
+InstanceId="$(aws ec2 run-instances --image-id $ImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --subnet-id $SubnetId --key-name vockey --user-data file://setupInstance.sh --block-device-mappings file://volume.json --iam-instance-profile Arn=arn:aws:iam::512241751838:instance-profile/LabInstanceProfile,Name=LabInstanceProfile --monitoring Enabled=true --query "Instances[].[InstanceId]" --output text)"
+$InstanceId
+aws ec2 monitor-instances --instance-ids $InstanceId
+
+aws cloudwatch put-metric-alarm --alarm-name --alarm-description "Abnormal network packet traffic (potential brute-force/dos)" --metric-name NetworkPacketSln --namespace AWS/EC2 --statistic Average --period 1 --threshold 600000 --comparison-operator GreaterThanThreshold --dimensions "Name=InstanceId,Value=ami-08c40ec9ead489470" --evaluation-periods 1 --alarm-actions arn:aws:automate:ca-central-1:ec2:stop --unit Count
